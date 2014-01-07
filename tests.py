fc9c80c58e41a9418e55e639483b9d6f60f65d51
Thomas Schreiber <tom@rizu.fake>
2014-01-08 00:07:21 +0100

    remove IndentationStyleChecker tests

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import pylint
 
 from mock import patch
 
-from pinax.checkers.style import QuotationStyleChecker, IndentationStyleChecker
+from pinax.checkers.style import QuotationStyleChecker
 
 
 class QuotationTest(unittest.TestCase):
@@ -58,81 +58,5 @@ def function_called('some "inner" string', 23):
         tokens = tokenize.generate_tokens(s.readline)
         linter = pylint.lint.PyLinter()
         checker = QuotationStyleChecker(linter)
-        checker.process_tokens(tokens)
-        self.assertEquals(AddMessageMock.call_count, 0)
-
-
-class IndentationTest(unittest.TestCase):
-
-    @patch("pylint.lint.PyLinter.add_message")
-    def test_line_indent_does_not_match_block(self, AddMessageMock):
-        s = cStringIO.StringIO("""
-def some_func():
-    x = 100
-
-    y = x * 100
-    return y**2
-""")
-        tokens = tokenize.generate_tokens(s.readline)
-        linter = pylint.lint.PyLinter()
-        checker = IndentationStyleChecker(linter)
-        checker.process_tokens(tokens)
-        AddMessageMock.assert_called_with("C9901", 4, None, (4, 0))
-
-    @patch("pylint.lint.PyLinter.add_message")
-    def test_line_indent_does_not_match_nested_block(self, AddMessageMock):
-        s = cStringIO.StringIO("""
-def some_func():
-    x = 100
-
-    if True:
-
-        y = x * 100
-    else:
-        y = 2
-    return y**2
-""")
-        tokens = tokenize.generate_tokens(s.readline)
-        linter = pylint.lint.PyLinter()
-        checker = IndentationStyleChecker(linter)
-        checker.process_tokens(tokens)
-        AddMessageMock.assert_called_with("C9901", 6, None, (8, 4))
-
-
-    @patch("pylint.lint.PyLinter.add_message")
-    def test_line_overindented(self, AddMessageMock):
-        s = cStringIO.StringIO("""
-def some_func():
-    x = 100
-
-    if True:
-
-        y = x * 100
-    else:
-        y = 2
-    return y**2
-""")
-        tokens = tokenize.generate_tokens(s.readline)
-        linter = pylint.lint.PyLinter()
-        checker = IndentationStyleChecker(linter)
-        checker.process_tokens(tokens)
-        AddMessageMock.assert_called_with("C9901", 6, None, (8, 12))
-
-    @patch("pylint.lint.PyLinter.add_message")
-    def test_line_indent_success(self, AddMessageMock):
-        s = cStringIO.StringIO("""
-def some_func():
-    x = 100
-
-    if True:
-        y = x * 100
-    else:
-        y = 2
-
-    return y**2
-""")
-        tokens = tokenize.generate_tokens(s.readline)
-        linter = pylint.lint.PyLinter()
-        checker = IndentationStyleChecker(linter)
         checker.process_tokens(tokens)
         self.assertEquals(AddMessageMock.call_count, 0)
